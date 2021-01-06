c8b558ad2d55aace7bf9936573cd3ec5f2c20afc
Athiley Luciano <athileyluciano@gmail.com>
2021-01-06 16:00:01 +0100

    adding doses to cocktail show page

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,14 +1,19 @@
 class DosesController < ApplicationController
   def new
-    @cocktail = Cocktail.find(params[:id])
+    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new
   end
 
   def create
     @dose = Dose.new(dose_params)
-    @dose.save
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
 
-    redirect_to cocktail_path(@dose.cocktail)
+    if @dose.save
+      redirect_to cocktail_path(@dose.cocktail)
+    else
+      render :new
+    end
   end
 
   def destroy
@@ -16,5 +21,11 @@ class DosesController < ApplicationController
     @dose.destroy
 
     redirect_to dose_path
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :cocktail_id, :ingredient_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'cocktails', to: 'cocktails#index'
-  get 'cocktails/new', to: 'cocktails#new'
-  get 'cocktails/:id', to: 'cocktails#show', as: :cocktail
-  post 'cocktails', to: 'cocktails#create'
+  # get 'cocktails', to: 'cocktails#index'
+  # get 'cocktails/new', to: 'cocktails#new'
+  # get 'cocktails/:id', to: 'cocktails#show', as: :cocktail
+  # # post 'cocktails', to: 'cocktails#create'
 
-  get 'cocktails/:id/doses/new', to: 'doses#new', as: :doses
-  post 'cocktails/:id/doses', to: 'doses#create'
-  delete 'doses/:id', to: 'doses#destroy'
+  # get 'cocktails/:id/doses/new', to: 'doses#new'
+  # post 'cocktails/:id/doses', to: 'doses#create'
+  # delete 'doses/:id', to: 'doses#destroy'
+
+  resources :cocktails do
+    resources :doses, only: [ :new, :create, :destroy ]
+  end
 
 end
