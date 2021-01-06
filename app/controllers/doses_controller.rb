a9fb40c826faad5c1bdb8b318659317554e92d08
Athiley Luciano <athileyluciano@gmail.com>
2021-01-06 15:36:58 +0100

    simple form for and fixing bugs

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,20 @@
 class DosesController < ApplicationController
+  def new
+    @cocktail = Cocktail.find(params[:id])
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @dose.save
+
+    redirect_to cocktail_path(@dose.cocktail)
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+
+    redirect_to dose_path
+  end
 end
