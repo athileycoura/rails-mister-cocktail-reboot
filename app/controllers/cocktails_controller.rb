d6ba9ce5f259108799a6cd2e7842fe4fb609e51b
Athiley Luciano <athileyluciano@gmail.com>
2021-01-08 19:41:18 +0100

    font awesome

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -18,6 +18,17 @@ class CocktailsController < ApplicationController
     redirect_to cocktail_path(@cocktail)
   end
 
+  def edit
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def update
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.update(cocktail_params)
+
+    redirect_to cocktail_path(@cocktail)
+  end
+
   def destroy
     @cocktail = Cocktail.find(params[:id])
     @cocktail.destroy
