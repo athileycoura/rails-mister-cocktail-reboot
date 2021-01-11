3301450e0ad8afb6b323ce9e8e9eea8f88b90567
Athiley Luciano <athileyluciano@gmail.com>
2021-01-11 16:35:06 +0100

    creating home page

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   # get 'cocktails/:id/doses/new', to: 'doses#new'
   # post 'cocktails/:id/doses', to: 'doses#create'
   # delete 'doses/:id', to: 'doses#destroy'
+  root 'pages#home'
 
   resources :cocktails do
     resources :doses, only: [ :new, :create, :edit, :update, :destroy ]
